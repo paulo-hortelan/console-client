7687b89894b9a1310efa01380e50072e830bfed2
paulo-hortelan <paulo.ribeiro@aonet.com.br>
2024-08-27 16:25:08 -0300

    fix: datacom

--- src/Helpers/Datacom.php
+++ src/Helpers/Datacom.php
@@ -4,13 +4,12 @@ namespace Meklis\Network\Console\Helpers;
 
 class Datacom extends DefaultHelper
 {
-    protected $prompt = '[>#]';
-    protected $userPrompt = 'Username:';
+    protected $prompt = '[#]';
+    protected $userPrompt = 'login:';
     protected $passwordPrompt = 'Password:';
     protected $eol = "\n";
     protected $doubleLoginPrompt = false;
     protected $enableMagicControl = false;
     protected $stripPrompt = true;
     protected $windowSize = null;
-    protected $waitingResponseTimeout = 1;
 }
